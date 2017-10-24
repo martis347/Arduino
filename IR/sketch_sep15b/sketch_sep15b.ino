722c18ba3373a935fc41f2e1fa715987096a67df
Martynas Kanapinskas <Martynas.Kanapinskas>
2017-10-24 22:40:21 +0300

    Added Infrared Scripts

--- IR/sketch_sep15b/sketch_sep15b.ino
+++ IR/sketch_sep15b/sketch_sep15b.ino
@@ -23,7 +23,7 @@ const int sound = 8;
 #define b2 13 //7
 
 int number1 = 30; //brightness or RED
-char mode = 'w';
+String mode = "w";
 bool power = false;
 int number2 = 0;
 int number3; //rainbowSpeed or BLUE
@@ -34,9 +34,10 @@ ESP8266WiFiMulti WiFiMulti;
 ESP8266WebServer server = ESP8266WebServer(80);
 WebSocketsServer webSocket = WebSocketsServer(81);
 
-IRrecv irrecv(2);
-
-
+decode_results results;//create instance of 'decode_results'
+decode_results prevResult;
+uint16_t RECV_PIN = 2;
+IRrecv irrecv(RECV_PIN);
 String html;
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght) {
     switch(type) {
@@ -94,16 +95,17 @@ void setup() {
       //prepareFiles();
 
     WiFi.begin("2 Guys 1 Router", "admin01ADF");
-    while (WiFi.status() != WL_CONNECTED) {
+    /*while (WiFi.status() != WL_CONNECTED) {
       delay(500);
       Serial.print(".");
-    }
+    }*/
    
     Serial.println("");
     Serial.println("WiFi connected");  
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
 
+    irrecv.enableIRIn();  // Start the receiver
     
     webSocket.begin();
     webSocket.onEvent(webSocketEvent);
@@ -119,59 +121,7 @@ void setup() {
 void loop() {
   webSocket.loop();
   server.handleClient();
-
-  if(Serial.available()) {
-    delay(100);
-    if((char)Serial.read() == '1')
-    {
-      power = true;
-    }
-    else
-    {
-      power = false;
-    }
-    Serial.read();
-    mode = (char)Serial.read(); Serial.read();
-    char a = Serial.read();
-    String number;
-    int i = 0;
-    while(isDigit(a)){
-      number += (char)a;
-      a = Serial.read();
-    }
-    number1 = number.toInt();
-    a = Serial.read();
-    i = 0;
-    number = "";
-    while(isDigit(a)){
-      number += (char)a;
-      a = Serial.read();
-    }
-    number2 = number.toInt();
-
-    a = Serial.read();
-    i = 0;
-    number = "";
-    while(isDigit(a)){
-      number += (char)a;
-      a = Serial.read();
-    }
-    number3 = number.toInt();
-    while(Serial.available())
-      Serial.read();
-  }
-  
-  signal = GenerateValuesString();
-  ProcessLights();
-  
-  if(broadcast)
-  {
-    Serial.print("Broadcasting "); 
-    Serial.println(signal);  
-    webSocket.broadcastTXT(signal);
-    broadcast = false;
-  }
-
+  IRsignal();
 }
 
 String GenerateValuesString()
@@ -223,44 +173,44 @@ void ProcessLights()
 {
   if(power)
   {
-    if(mode == 'w')
+    if(mode == "w")
     {
       analogWrite(r1, number1); analogWrite(r2, number1);
       analogWrite(g1, number1); analogWrite(g2, number1);
       analogWrite(b1, number1); analogWrite(b2, number1);
     }
-    else if (mode == 'r')
+    else if (mode == "r")
     {
       analogWrite(r1, number1); analogWrite(r2, number1);
       analogWrite(g1, 0); analogWrite(g2, 0);
       analogWrite(b1, 0); analogWrite(b2, 0);
     }
-    else if (mode == 'g')
+    else if (mode == "g")
     {
       analogWrite(r1, 0); analogWrite(r2, 0);
       analogWrite(g1, number1); analogWrite(g2, number1);
       analogWrite(b1, 0); analogWrite(b2, 0);
     }
-    else if (mode == 'b')
+    else if (mode == "b")
     {
       analogWrite(r1, 0); analogWrite(r2, 0);
       analogWrite(g1, 0); analogWrite(g2, 0);
       analogWrite(b1, number1); analogWrite(b2, number1);
     }
     
-    else if (mode == '~')
+    else if (mode == "~")
     {
       analogWrite(r1, r); analogWrite(r2, r);
       analogWrite(g1, g); analogWrite(g2, g);
       analogWrite(b1, b); analogWrite(b2, b);
     }
-    else if (mode == 'a')
+    else if (mode == "a")
     {
       analogWrite(r1, number1); analogWrite(r2, number1);
       analogWrite(g1, number2); analogWrite(g2, number2);
       analogWrite(b1, number3); analogWrite(b2, number3);
     }
-    else if (mode == 'm')
+    else if (mode == "m")
     {
       bool val = digitalRead(sound);
       unsigned int r, g, b;
@@ -289,7 +239,7 @@ void ProcessLights()
     analogWrite(b1, 0); analogWrite(b2, 0);
   }
 
-  if (mode == '~' && millis() - mils >= number3 / 4)
+  if (mode == "~" && millis() - mils >= number3 / 4)
   {
     number2++;
     if(number2 >= 1024)
@@ -305,32 +255,253 @@ void parseNetworkMessage(char* message)
   int i = 0;
   while((token = strtok_r(message, " ", &message)))
   {
-  	if(i == 0)
-  	{
-  		power = atoi(token) == 1 ? true : false;  
-  	}
-  	else if(i == 1)
-  	{
-  		mode = token[0];
-  	}
-  	else if(i == 2)
-  	{
+    if(i == 0)
+    {
+      power = atoi(token) == 1 ? true : false;  
+    }
+    else if(i == 1)
+    {
+      mode = token[0];
+    }
+    else if(i == 2)
+    {
       number1 = atoi(token);
-  	}
-  	else if(i == 3 && mode == 'a')
-  	{
-  		number2 = atoi(token);
-  	}
-  	else if(i == 4)
-  	{
-  		number3 = atoi(token);   
-  	}
+    }
+    else if(i == 3 && mode == "a")
+    {
+      number2 = atoi(token);
+    }
+    else if(i == 4)
+    {
+      number3 = atoi(token);   
+    }
     else if(i == 5)
     {
       broadcast = atoi(token) == 1 ? true : false;   
     }
-  	i++;
+    i++;
   }
+}
+
+void IRsignal() //takes action based on IR code received
+{
+  if (results.value != 0xFFFFFFFF)
+    prevResult = results;
+  if (irrecv.decode(&results)) //check if it is received an IR signal
+  {
+    
+    
+      //Serial.println(results.value, HEX);//print on Serial monitor the value received in hexadecimal.Read the value of the button that you want use and put it into the switch case
+  
+      
+    if (ProcessIR())
+    {
+      signal = GenerateValuesString();
+      ProcessLights();
+      
+      if(broadcast)
+      {
+        Serial.print("Broadcasting "); 
+        Serial.println(signal);  
+        webSocket.broadcastTXT(signal);
+        broadcast = false;
+      }
+    }
+    irrecv.resume(); //receive the next value
+  }
+}
+
+bool ProcessIR() {
+  switch (results.value) //check the IR signal received
+  {
+    case 0xFFA05F:
+      if (number1 <100)
+      {
+        number1 += 20;
+      }
+      else
+      {
+        number1 += 50;
+      }
+      if (number1 > 255)
+        number1 = 255;
+      //Serial.print("Brighter - ");
+      //Serial.println(brightness);
+      break;
+    case 0xFF20DF:
+      if (number1 <101)
+      {
+        number1 -= 20;
+      }
+      else
+      {
+        number1 -= 50;
+      }
+      if (number1 < 0)
+        number1 = 0;
+      //Serial.print("Un-brighter - ");
+      //Serial.println(brightness);
+      break;
+    case 0xFF906F:
+      mode = "r";
+      //Serial.println("Red");
+      break;
+    case 0xFF10EF:
+      mode = "g";
+      //Serial.println("Green");
+      break;
+    case 0xFF50AF:
+      mode = "b";
+      //Serial.println("Blue");
+      break;
+    case 0xFFD02F:
+      mode = "w";
+      //Serial.println("White");
+      break;
+    case 0xFFE01F:
+      if (!power) {
+        power = true;
+        number1 = 30;
+        mode = "w";
+        //Serial.println("On");
+      }
+      break;
+    case 0xFF609F:
+      if (power)
+      {
+        power = false;
+        number1 = 0;
+        mode = "w";
+        //Serial.println("Off");
+      }
+      break;
+    case 0xFFC837:
+      //Serial.println("Rainbow");
+      mode = "~";
+      break;
+    case 0xFFF00F: //case for music
+      mode = "m";
+      break;
+    case 0xFFE817: //case for rgb
+      mode = "a";
+      number1 = 30;
+      number2 = 30;
+      number3 = 30;
+      break;
+    case 0xFFB04F: //case for R+
+      if(mode == "a")
+      {
+        number1 += 50;
+        if (number1 > 255)
+          number1 = 255;
+      }
+      break;
+    case 0xFF30CF: //case for G+
+      if(mode == "a")
+      {
+        number2 += 50;
+        if (number2 > 255)
+          number2 = 255;
+      }
+      break;
+    case 0xFF708F: //case for B+
+      if(mode == "a")
+      {
+        number3 += 50;
+        if (number3 > 255)
+          number3 = 255;
+      }
+      break;
+    case 0xFFA857: //case for R-
+      if(mode == "a")
+      {
+        number1 -= 50;
+        if (number1 < 0)
+          number1 = 0;
+      }
+      break;
+    case 0xFF28D7: //case for G- 
+      if(mode == "a")
+      {
+        number2 -= 50;
+        if (number2 < 0)
+          number2 = 0;
+      }
+      break;
+    case 0xFF6897: //case for B- 
+      if(mode == "a")
+      {
+        number3 -= 50;
+        if (number3 < 0)
+          number3 = 0;
+      }
+      break;
+    case 0xFF48B7: //rainbow speed up
+      if(mode == "~")
+      {
+         number3 += 50;
+      }
+      if (number3 > 200)
+        number3 = 200;
+        break;
+    case 0xFF08F7:  //rainbow speed down
+      if(mode == "~")
+      {
+         number3 -= 50;
+      }
+      if (number3 < 0)
+        number3 = 0;
+        break;
+    case 0xFFFFFFFF:
+      if (prevResult.value == 0xFFA05F && number1 < 250)
+      {
+        number1 += 6;
+        //Serial.print("Little brighter - ");
+        //Serial.println(brightness);
+      }
+      else if (prevResult.value == 0xFF20DF  && number1 > 5)
+      {
+        number1 -= 6;
+        //Serial.print("Little un-brighter - ");
+        //Serial.println(brightness);
+      }
+      else if (prevResult.value == 0xFFB04F  && number1 < 250 && mode == "a")
+      {
+         number1 += 6;
+      }
+      else if (prevResult.value == 0xFFA857  && number1 > 5 && mode == "a")
+      {
+         number1 -= 6;
+      }
+       else if (prevResult.value == 0xFF30CF  && number2 < 250 && mode == "a") 
+      {
+         number2 += 6;
+      }
+      else if (prevResult.value == 0xFF28D7  && number2 > 5 && mode == "a")
+      {
+         number2 -= 6;
+      }
+       else if (prevResult.value == 0xFF708F  && number3 < 250 && mode == "a")
+      {
+         number3 += 6;
+      }
+      else if (prevResult.value == 0xFF6897  && number3 > 5 && mode == "a")  
+      {
+         number3 -= 6;
+      }
+      else if (prevResult.value == 0xFF08F7  && number3 > 4 && mode == "~")  
+      {
+         number3 -= 5;
+      }
+      else if (prevResult.value == 0xFF48B7  && number3 < 196 && mode == "~")  
+      {
+         number3 += 5;
+      }
+      break;
+    default:
+      return false;
+  }
+  return true;
 }
 
 void HSBToRGB(
